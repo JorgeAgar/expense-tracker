4a18d8dcf92e6f0f390704896e03896cbd1a829b
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-22 09:23:42 -0500

    Add main.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Movement } from './components/Movement'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <p>Hello World</p>
+    <Movement />
   </StrictMode>,
 )

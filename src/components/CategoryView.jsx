270b87e42d1b90ca742d063e73321fd17336af9b
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-23 13:53:59 -0500

    Update CategoryView

--- src/components/CategoryView.jsx
+++ src/components/CategoryView.jsx
@@ -1,18 +1,13 @@
-import { PieChart } from "recharts";
-import { Pie } from "recharts";
+import CustomPieChart from "./CustomPieChart";
 
 const data = [
-    {"name": "Name a", "value": 300, "fill": "#FF0000"},
-    {"name": "Name b", "value": 300, "fill": "#00FF00"},
-    {"name": "Name c", "value": 300, "fill": "#0000FF"},
-]
+  { name: "Name a", value: 200, fill: "#FF0000" },
+  { name: "Name b", value: 400, fill: "#00FF00" },
+  { name: "Name c", value: 300, fill: "#0000FF" },
+];
 
 const CategoryView = () => {
-  return (
-    <PieChart width={400} height={400}>
-      <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} innerRadius={50} />
-    </PieChart>
-  );
-}
+  return <CustomPieChart data={data} />;
+};
 
 export default CategoryView;

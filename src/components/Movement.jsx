aae04c9b6b49d876753a55ea67bf99908451186d
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-23 12:43:42 -0500

    prop name change

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -1,7 +1,7 @@
-export function Movement({ expense, value }) {
+export function Movement({ name, value }) {
   return (
-    <div className="inline-flex flex-col items-start justify-start border border-gray-700 text-gray-700 rounded-2xl p-2 bg-gray-200 shadow-sm max-w-max">
-      <p className="text-xs font-medium">{expense}</p>
+    <div className="inline-flex flex-col items-start justify-start border-gray-700 border-2 text-gray-700 rounded-2xl p-2 bg-gray-200 shadow-sm max-w-max">
+      <p className="text-xs font-medium">{name}</p>
       <p className="mt-1 text-sm font-semibold">{value}</p>
     </div>
   );

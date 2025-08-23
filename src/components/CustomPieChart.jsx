85d19344ea9d017fef9aa5a75ac52969b43cbbac
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-23 12:43:29 -0500

    CustomPieChart uses Movement component as a label when hovering

--- src/components/CustomPieChart.jsx
+++ src/components/CustomPieChart.jsx
@@ -1,6 +1,19 @@
-import { PieChart } from "recharts";
-import { Pie } from "recharts";
-import { Tooltip } from "recharts";
+import { PieChart, Pie, Tooltip } from "recharts";
+import { Movement } from "./Movement";
+
+const CustomTooltip = ({ active, payload }) => {
+  const isVisible = active && payload && payload.length;
+  return (
+    <div
+      className="custom-tooltip"
+      style={{ visibility: isVisible ? "visible" : "hidden" }}
+    >
+      {isVisible && (
+        <Movement name={payload[0].name} value={payload[0].value} />
+      )}
+    </div>
+  );
+};
 
 const CustomPieChart = ({ data }) => {
   return (
@@ -14,7 +27,7 @@ const CustomPieChart = ({ data }) => {
         outerRadius={"100%"}
         innerRadius={"50%"}
       />
-      <Tooltip />
+      <Tooltip content={<CustomTooltip />} />
     </PieChart>
   );
 };

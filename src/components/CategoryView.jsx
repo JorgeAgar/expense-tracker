49c6b869196b15e9ce456bdd0f16cef2ed0aa411
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-23 11:25:26 -0500

    test pie rendering

--- src/components/CategoryView.jsx
+++ src/components/CategoryView.jsx
@@ -1,5 +1,18 @@
+import { PieChart } from "recharts";
+import { Pie } from "recharts";
+
+const data = [
+    {"name": "Name a", "value": 300, "fill": "#FF0000"},
+    {"name": "Name b", "value": 300, "fill": "#00FF00"},
+    {"name": "Name c", "value": 300, "fill": "#0000FF"},
+]
+
 const CategoryView = () => {
-  return <div>Category View Component</div>;
+  return (
+    <PieChart width={400} height={400}>
+      <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} innerRadius={50} />
+    </PieChart>
+  );
 }
 
 export default CategoryView;

58d438928318f9041898c5c2d9a5bfad9b97b9e5
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-08-25 15:58:17 -0500

    Initial SankeyChart implementation

--- src/components/CustomSankeyChart.jsx
+++ src/components/CustomSankeyChart.jsx
@@ -1,5 +1,24 @@
-const CustomSankeyChart = () => {
-    
+import { Sankey, Tooltip } from "recharts";
+
+const CustomSankeyChart = ({ data }) => {
+  return (
+    <Sankey
+      width={960}
+      height={500}
+      data={data}
+      node={{stroke: "#77c878", strokeWidth: 2}}
+      nodePadding={50}
+      margin={{
+        left: 200,
+        right: 200,
+        top: 100,
+        bottom: 100,
+      }}
+      link={{ stroke: "#77c878" }}
+    >
+      <Tooltip />
+    </Sankey>
+  );
 };
 
 export default CustomSankeyChart;
